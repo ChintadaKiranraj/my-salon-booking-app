e96ca82909bcf787ccc5539021a9b44dad846345
Chintada Kiranraj <kiranrajchintada302@gmail.com>
2024-03-06 09:22:04 +0530

    NewApproach

--- src/App.jsx
+++ src/App.jsx
@@ -16,19 +16,36 @@ import Header from "./components/Header";
 import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
 import "@coreui/coreui/dist/css/coreui.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import myProfilePick from "./Images/loginbg.svg";
+import myProfilePick2 from "./Images/profile.svg";
+import Homepage from "./components/Homepage/Homepage";
+import Barber from "./components/Barbars";
+import Bookings from "./components/Bookings";
+import UserBookings from "./components/UserBookings/userbookings";
+
 const App = () => (
   <Router>
     <div className="App">
       <Switch>
-        <Route exact path="/login" component={LoginForm} />
+        {/* <img alt="profilePick" src={myProfilePick2} id="profilepic" /> */}
+
+        {/* <Route exact path="/login" component={LoginForm} />
         <Route exact path="/registartion" component={SignupForm} />
-        <ProtectedRoute exact path="/" component={Home} />
+        <ProtectedRoute exact path="/home" component={Home} />
         <ProtectedRoute exact path="/admin" component={Admin} />
         <ProtectedRoute exact path="/barbers" component={Barbars} />
         <ProtectedRoute exact path="/approvals" component={Approvals} />
-        <ProtectedRoute exact path="/user" component={ViewOnly} />
-        <Route path="/not-found" component={NotFound} />
-        <Redirect to="/not-found" />
+        <ProtectedRoute exact path="/user" component={ViewOnly} /> */}
+        {/* <Route path="/not-found" component={NotFound} /> */}
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/login" component={LoginForm} />
+        <Route exact path="/register/shop-owner" component={SignupForm} />
+        <Route exact path="/register/customer" component={SignupForm} />
+        <ProtectedRoute exact path="/home" component={Barber} />
+        <ProtectedRoute exact path="/shop/schedules" component={Bookings} />
+        <ProtectedRoute exact path="/user/schedules" component={UserBookings} />
+
+        <Route component={NotFound} />
       </Switch>
     </div>
   </Router>
---
+++ src/components/Bookings/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Admin from "../Admin";
+
+const Bookings = () => {
+  return (
+    <div>
+      <p>
+        we display here the no of booking the shop the shop admin can see the
+        how many of the booking was happen and how many was pending and he can
+        reject approvala and delete the bookings
+      </p>
+      <p>i need to display the booking details table here</p>
+
+      <Admin />
+    </div>
+  );
+};
+
+export default Bookings;
