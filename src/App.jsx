c0910a189f0bfcc58f2f7af9bf86e6e2b3c279fb
kiranraj.chintada <kiranraj.chintada@epsoftinc.com>
2024-03-27 21:41:05 +0530

    removed the un used imports in App.js component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./components/Home";
-import Admin from "./components/Admin";
 import LoginForm from "./components/LoginForm";
 import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 import NotFound from "./components/NotFound";
-import SignupForm from "./components/SignupForm";
-import Barbars from "./components/BarberManagement/Barber/Barber";
 import ViewOnly from "./components/Viewonly";
-
-
 import "./App.css";
-import Approvals from "./components/Approvals/Approvals";
-import Header from "./components/Header/header";
-import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
 import "@coreui/coreui/dist/css/coreui.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import Barber from "./components/BarberManagement/Barber/Barber";
 import BarberApplicationsForm from "./components/BarberManagement/ApplicationForm/ApplicationForm";
 import BarberApplicationData from "./components/BarberManagement/ApplicationsList/ApplicationsList";
@@ -25,43 +15,17 @@ import Bookings from "./components/Bookings";
 import Shops from "./components/Shops/ShopsList/ShopsList";
 import ShopOwners from "./components/Shops/ShopOwners/shopowner";
 import Users from "./components/UserManagment/Users/users";
-// import Appointment from "./components/Appointment/appointment";
-
 import ShopRegistrationForm from "./components/Shops/ShopRegistrationForm/shopregistrationform";
-
 import Welcome from "./components/Welcome/Welcome";
 import AdminOne from "./components/AdminOne/AdminOne";
-
 import ShpoRegistraction from "./components/ShopsTwo/shopTwo";
-
-// import BookedAppointments from "./components/BookedAppointments";
-import EditShopsData from "./components/Shops/EditShops/EditShopsData";
 import MyAppointments from "./components/UserManagment/MyAppointments/MyAppointments";
 import Appointment from "./components/UserManagment/AppointmentForm/appointment";
-
-
-
-
-
 
 const App = () => (
   <Router>
     <div className="App">
       <Switch>
-      {/* <ProtectedRoute exact path="/home" component={Home} /> */}
-        {/* <img alt="profilePick" src={myProfilePick2} id="profilepic" /> */}
-
-        {/* <Route exact path="/login" component={LoginForm} />
-        <Route exact path="/registartion" component={SignupForm} />
-        <ProtectedRoute exact path="/home" component={Home} />
-        <ProtectedRoute exact path="/admin" component={Admin} />
-        <ProtectedRoute exact path="/barbers" component={Barbars} />
-        <ProtectedRoute exact path="/approvals" component={Approvals} />
-        // <ProtectedRoute exact path="/user" component={ViewOnly} /> */}
-        {/* <Route path="/not-found" component={NotFound} /> */}
-        {/* <ProtectedRoute exact path="/barberregistration" component={UserBookings} /> */}
-        {/* <ProtectedRoute exact path="/user/schedules" component={UserBookings} /> */}
-      
         <Route exact path="/" component={Welcome} />
         <Route exact path="/login" component={LoginForm} />
         <ProtectedRoute exact path="/user" component={ViewOnly} /> 
@@ -69,8 +33,6 @@ const App = () => (
         <ProtectedRoute exact path="/shopowners" component={ShopOwners} />
         <ProtectedRoute exact path="/users" component={Users} />
         <ProtectedRoute exact path="/noofbarbers" component={Barber} />
-        {/* <ProtectedRoute exact path="/userbookings" component={Bookings} /> */}
-        
         <ProtectedRoute exact path="/barberregistration" component={BarberRegistrationForm} />
         <ProtectedRoute exact path="/shopregistrationForm" component={ShopRegistrationForm} />
         <ProtectedRoute exact path="/BarberApplicationsForm" component={BarberApplicationsForm} />
